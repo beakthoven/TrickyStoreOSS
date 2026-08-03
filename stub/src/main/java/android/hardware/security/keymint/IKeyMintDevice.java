33d579e85e3f584c479b6bd0b0078459eaa6fe73
lewisky <vladelewisky@gmail.com>
2026-08-03 14:42:47 -0600

    build: add compile-only KeyMint binder stub

---
+++ stub/src/main/java/android/hardware/security/keymint/IKeyMintDevice.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2026 Dakkshesh <beakthoven@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package android.hardware.security.keymint;
+
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.RemoteException;
+
+/** Compile-only stub for the platform KeyMint stable AIDL interface. */
+public interface IKeyMintDevice extends IInterface {
+    String DESCRIPTOR = "android.hardware.security.keymint.IKeyMintDevice";
+
+    int getInterfaceVersion() throws RemoteException;
+
+    abstract class Stub {
+        public static IKeyMintDevice asInterface(IBinder binder) {
+            throw new UnsupportedOperationException("");
+        }
+    }
+}
